08595e55c84a1fdd62eb0db508a11332de59fc12
samjjc <sam111jc@gmail.com>
2019-02-16 14:57:32 -0500

    add new activity

--- app/src/main/java/com/example/johnny/workshop/MainActivity.kt
+++ app/src/main/java/com/example/johnny/workshop/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.johnny.workshop
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,9 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var count = 0
-        plus.setOnClickListener { view ->
+
+        plus.setOnClickListener {
             count++
             text.text = count.toString()
+        }
+
+        minus.setOnClickListener {
+            count--
+            text.text = count.toString()
+        }
+
+        next.setOnClickListener {
+            val intent = Intent(this, Part2::class.java)
+            startActivity(intent)
         }
     }
 }
