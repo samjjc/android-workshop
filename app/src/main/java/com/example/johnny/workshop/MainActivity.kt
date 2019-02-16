f98d7e9f6696e147eb6b0d89bd7491afeb7caf50
samjjc <sam111jc@gmail.com>
2019-02-16 13:58:13 -0500

    add incrementor

--- app/src/main/java/com/example/johnny/workshop/MainActivity.kt
+++ app/src/main/java/com/example/johnny/workshop/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.johnny.workshop
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var count = 0
+        plus.setOnClickListener { view ->
+            count++
+            text.text = count.toString()
+        }
     }
 }
